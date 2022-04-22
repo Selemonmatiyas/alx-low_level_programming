170da4ffe3abb27410426d3bd103350be6ebc25f
Selemonmatiyas <selemonmatiyas27@gmail.com>
2022-04-22 09:53:05 -0700

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,5 +10,4 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
-
 #endif
