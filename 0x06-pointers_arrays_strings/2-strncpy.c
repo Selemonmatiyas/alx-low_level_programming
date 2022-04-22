67aac5c74aab0cc50e5471e74e3783870af934ae
Selemonmatiyas <selemonmatiyas27@gmail.com>
2022-04-22 21:55:18 +0530

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 
         for (x = 0; x < n && src[x] != '\0'; x++)
                 dest[x] = src[x];
-        for (; i > n; i++)
-                dest[i] = '\0';
+        for (; x > n; x++)
+                dest[x] = '\0';
 
         return (dest);
 }
