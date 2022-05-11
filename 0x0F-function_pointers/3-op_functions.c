01e8b447421a1fd528907b241b1e74e9004fbafe
Selemonmatiyas <selemonmatiyas27@gmail.com>
2022-05-11 23:34:07 +0530

    My commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -25,11 +25,21 @@ int op_sub(int a, int b)
  * op_mul - 5 functions
  * @a: input one
  * @b: input two
- * Return:
+ * Return: 0
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
+}
+/**
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
 }
 /**
  * op_mod -5 functions
